de1a2fceb808571059d4ac9c3e034b77a972b24f
mculotta120 <margaret.culotta@gmail.com>
2016-09-15 21:49:55 -0500

    modal is now working, but code is sloppy right now

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,7 +1,7 @@
 console.log("client.js is sourced");
 
 //create an ng app for the page
-var myApp = angular.module( 'myApp', ['ngRoute', 'ui.bootstrap' ]);
+var myApp = angular.module( 'myApp', ['ngRoute', 'ui.bootstrap', 'ngAnimate', 'ngSanitize' ]);
 //create a controller
 myApp.config(['$routeProvider', function($routeProvider){
   $routeProvider.
@@ -98,38 +98,3 @@ console.log(objectToEdit, "object");
 }; // end editChar
 
 }]); //end charDisplayController
-
-myApp.directive('modalDialog', function(  ) {
-  return {
-    restrict: 'E',
-    scope: {
-      show: '='
-    },
-    replace: true, // Replace with the template below
-    transclude: true, // we want to insert custom content inside the directive
-    link: function(scope, element, attrs) {
-      scope.dialogStyle = {};
-      if (attrs.width)
-        scope.dialogStyle.width = attrs.width;
-      if (attrs.height)
-        scope.dialogStyle.height = attrs.height;
-      scope.hideModal = function() {
-        scope.show = false;
-      };
-    },
-    template: "<div class='ng-modal' ng-show='show'><div class='ng-modal-overlay' ng-click='hideModal()'></div><div class='ng-modal-dialog' ng-style='dialogStyle'><div class='ng-modal-close' ng-click='hideModal()'>X</div><div class='ng-modal-dialog-content' ng-transclude></div></div></div>"
-  };
-});
-
-myApp.controller('MyCtrl', ['$scope', function($scope) {
-  $scope.modalShown = false;
-  $scope.toggleModal = function( ) {
-    $scope.modalShown = !$scope.modalShown;
-  };
-  $scope.openModal = function( ){
-    console.log("openModal clicked");
-
-  }; // end openModal
-
-
-}]);
